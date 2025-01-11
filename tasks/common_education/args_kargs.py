7886fcf53723938745ed390c5890969cbcffbf13
teodor-r <73360513+teodor-r@users.noreply.github.com>
2025-01-11 10:11:06 +0300

    Delete tasks/common_education directory

--- tasks/common_education/args_kargs.py
+++
@@ -1,7 +0,0 @@
-
-def func( *args, **kargs):
-    print(list(args))
-    print(kargs)
-
-
-func(1,23,4, name = "na,e")
--- tasks/common_education/copy_deepcopy.py
+++
@@ -1,4 +0,0 @@
-import copy
-xs = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-zs = copy.deepcopy(xs)
-
